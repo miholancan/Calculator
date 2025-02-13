c2613f2d4de74459f433c5dba32646bc829a1393
miholancan <igormiholancan@gmail.com>
2025-02-13 08:21:14 +0000

    Added basic functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function sum(n1, n2){
-	return n1 + n2;
+	return Number(n1) + Number(n2);
 }
 function sub(n1, n2){
 	return n1 - n2;
@@ -8,7 +8,11 @@ function mul(n1, n2){
 	return n1 * n2;
 }
 function div(n1, n2){
-	return n1 / n2;
+	if (n2 === "0") {
+		return display.textContent = "ERROR:Can't divide by 0"
+	}else {
+		return n1 / n2;
+	}
 }
 function operate(operator, n1, n2){
 	if(operator === "+") {
@@ -29,21 +33,55 @@ function operate(operator, n1, n2){
 }
 
 let n1 = "";
-
+let n2 = "";
+let operator = "";
+const operatorButton = ["Divide", "Multiply", "Subtract", "Add"];
+const operatorSymbol = ["/", "*", "-", "+"];
 let display = document.querySelector(".displayWrapper");
-let no1 = document.querySelector("button.button1");
-no1.onclick = () => {
-	n1 = n1.concat("1");
-	display.textContent = n1;
 
-	console.log(n1);
-};
 
+for(let i = 0; i <= 9; i++){
+	let button = document.querySelector(`button.button${i}`);
+	button.onclick = () => {
+		if (operator.length < 1) {
+			n1 = n1.concat(i);
+			display.textContent = n1;
+			console.log(`This is n1 -> ${n1}`);
+		}else {
+			n2 = n2.concat(i);
+			display.textContent = n1 + "" + operator + "" + n2;
+			console.log(`This is n2 -> ${n2}`);
+		}
+	};
+}
+
+for(let i = 0; i <= 3; i++){
+	let button = document.querySelector(`button.button${operatorButton[i]}`);
+	button.onclick = () => {
+		if (operatorSymbol[i] === "-" && n1.length < 1) {
+			n1 = operatorSymbol[i];
+			display.textContent = n1 + "" + operator;
+		}
+		else if(operator.length < 1 && n1.length > 0){
+			operator = operatorSymbol[i];
+			display.textContent = n1 + "" + operator;
+		}	
+	}
+}
+
+
+let equalsButton = document.querySelector("button.buttonEquals");
+equalsButton.onclick = () => {
+	let result = operate(operator, n1, n2);
+	display.textContent = result;
+}
 
 let allClear = document.querySelector("button.buttonClear");
 allClear.onclick = () => {
 	n1 = "";
-	display.textContent = n1;
+	n2 = "";
+	operator = "";
+	display.textContent = "";
 	console.log(n1);
 }
 
