fa497fe8bec82e5587b5f909c8dafa20b24342e1
miholancan <igormiholancan@gmail.com>
2025-02-18 19:25:34 +0000

    Improved functionality

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function sum(n1, n2){
-	return Number(n1) + Number(n2);
+function sum(n1, n2, decimalPlaces = 8){
+	let n3 = Number(n1) + Number(n2);
+	return parseFloat(n3.toString().match(new RegExp(`^-?\\d+(?:\\.\\d{0,${decimalPlaces}})?`))[0]);
 }
-function sub(n1, n2){
-	return n1 - n2;
+function sub(n1, n2, decimalPlaces = 8){
+	let n3 = n1 - n2;
+	return parseFloat(n3.toString().match(new RegExp(`^-?\\d+(?:\\.\\d{0,${decimalPlaces}})?`))[0]);
 }
-function mul(n1, n2){
-	return n1 * n2;
+function mul(n1, n2, decimalPlaces = 8){
+	let n3 = n1 * n2;
+	return parseFloat(n3.toString().match(new RegExp(`^-?\\d+(?:\\.\\d{0,${decimalPlaces}})?`))[0]);
 }
-function div(n1, n2){
+function div(n1, n2, decimalPlaces = 8){
 	if (n2 === "0") {
-		return display.textContent = "ERROR:Can't divide by 0"
+		return display.textContent = "ERROR"
 	}else {
-		return n1 / n2;
+		let n3 = n1 / n2;
+		return parseFloat(n3.toString().match(new RegExp(`^-?\\d+(?:\\.\\d{0,${decimalPlaces}})?`))[0]);
 	}
 }
 function operate(operator, n1, n2){
@@ -31,40 +35,122 @@ function operate(operator, n1, n2){
 		return "Error";
 	}
 }
+function containsOperator(str){
+	return /[+\-*/]/.test(str);
+}
 
 let n1 = "";
 let n2 = "";
+let result = "";
 let operator = "";
+
 const operatorButton = ["Divide", "Multiply", "Subtract", "Add"];
 const operatorSymbol = ["/", "*", "-", "+"];
 let display = document.querySelector(".displayWrapper");
 
+const buttonDot = document.querySelector("button.buttonDot");
+buttonDot.onclick = () => {
+	if (result == display.textContent) {
+		n1 = "";
+		n2 = "";
+		result = "";
+		operator = "";
+		display.textContent = "";
+		buttonDot.disabled = false;
+		}
+	if (containsOperator(display.textContent)) {
+		if (n2 === "") {
+			n2 = "0"
+		}
+		n2 = n2.concat(".");
+		buttonDot.disabled = true;
+		display.textContent = n1 + "" + operator + "" + n2;
+	}
+	else {
+		if (n1 === "") {
+			n1 = "0"
+		}
+		n1 = n1.concat(".");
+		buttonDot.disabled = true;
+		display.textContent = n1;
+	}
+}
 
 for(let i = 0; i <= 9; i++){
 	let button = document.querySelector(`button.button${i}`);
 	button.onclick = () => {
-		if (operator.length < 1) {
-			n1 = n1.concat(i);
-			display.textContent = n1;
-			console.log(`This is n1 -> ${n1}`);
-		}else {
+		if (result == display.textContent || display.textContent == "ERROR") {
+			n1 = "";
+			n2 = "";
+			result = "";
+			operator = "";
+			display.textContent = "";
+			buttonDot.disabled = false;
+			}
+		if (result === "") {
+			if (operator.length < 1) {
+				if (n1 === "0") {
+					n1 = "0.";
+					buttonDot.disabled = true;
+				}
+				n1 = n1.concat(i);
+				display.textContent = n1;
+			}else {
+				if (n2 === "0") {
+					n2 = "0.";
+					buttonDot.disabled = true;
+				}
+				n2 = n2.concat(i);
+				display.textContent = n1 + "" + operator + "" + n2;
+			}
+		}
+		else {
+			if (n2 === "0") {
+				n2 = "0.";
+				buttonDot.disabled = true;
+			}
 			n2 = n2.concat(i);
+			n1 = result;
 			display.textContent = n1 + "" + operator + "" + n2;
-			console.log(`This is n2 -> ${n2}`);
+		}
+		if (display.textContent.length > 14) {
+			display.style.fontSize = "1.5rem";
 		}
 	};
 }
 
+
+
 for(let i = 0; i <= 3; i++){
 	let button = document.querySelector(`button.button${operatorButton[i]}`);
 	button.onclick = () => {
-		if (operatorSymbol[i] === "-" && n1.length < 1) {
-			n1 = operatorSymbol[i];
-			display.textContent = n1 + "" + operator;
+		buttonDot.disabled = false;
+		if (display.textContent == "ERROR") {
+			n1 = "";
+			n2 = "";
+			result = "";
+			operator = "";
+			display.textContent = "";
+			buttonDot.disabled = false;
 		}
-		else if(operator.length < 1 && n1.length > 0){
+		if (operator.length > 0) {
+			result = operate(operator, n1, n2);
+			display.textContent = result;
+		}
+		if (result === "") {
+			if (operatorSymbol[i] === "-" && n1.length < 1) {
+				n1 = operatorSymbol[i];
+				display.textContent = n1 + "" + operator;
+			}
+			else if(operator.length < 1 && n1.length > 0){
+				operator = operatorSymbol[i];
+				display.textContent = n1 + "" + operator;
+			}	
+		}
+		else {
+			n2 = "";
 			operator = operatorSymbol[i];
-			display.textContent = n1 + "" + operator;
+			display.textContent = result + "" + operator;
 		}	
 	}
 }
@@ -72,16 +158,24 @@ for(let i = 0; i <= 3; i++){
 
 let equalsButton = document.querySelector("button.buttonEquals");
 equalsButton.onclick = () => {
-	let result = operate(operator, n1, n2);
-	display.textContent = result;
+	if (n1 === "" || n2 === "" || operator === "") {
+		display.textContent = "ERROR";
+	}
+	else{
+		result = operate(operator, n1, n2);
+		display.textContent = result;
+	}
+	
 }
 
 let allClear = document.querySelector("button.buttonClear");
 allClear.onclick = () => {
 	n1 = "";
 	n2 = "";
+	result = "";
 	operator = "";
 	display.textContent = "";
-	console.log(n1);
+	buttonDot.disabled = false;
+	display.style.fontSize = "2rem";
 }
 
